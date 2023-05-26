b6ccdbb2302328818365294f167791cad2a09ba8
JiwonChoi <128335544+cjw0824@users.noreply.github.com>
2023-05-26 18:03:52 +0900

    feat:[최지원] 상세페이지 삭제기능 추가 [FPB-3]

--- src/router/product/index.js
+++ src/router/product/index.js
@@ -1,11 +1,12 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
+import Vue from "vue";
+import VueRouter from "vue-router";
 
-import ProductReadPage from "@/views/product/ProductReadPage.vue"
-import ProductListPage from "@/views/product/ProductListPage.vue"
-import ProductRegisterPage from "@/views/product/ProductRegisterPage.vue"
+import ProductReadPage from "@/views/product/ProductReadPage.vue";
+import ProductListPage from "@/views/product/ProductListPage.vue";
+import ProductRegisterPage from "@/views/product/ProductRegisterPage.vue";
+import ProductModifyPage from "@/views/product/ProductModifyPage.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const productRoutes = [
   {
@@ -23,6 +24,11 @@ const productRoutes = [
     name: "ProductRegisterPage",
     component: ProductRegisterPage,
   },
-]
+  {
+    path: "/productModifyPage",
+    name: "ProductModifyPage",
+    component: ProductModifyPage,
+  },
+];
 
-export default productRoutes
+export default productRoutes;
--- src/views/product/ProductReadPage.vue
+++ src/views/product/ProductReadPage.vue
@@ -3,20 +3,27 @@
     <h1>상품 상세페이지</h1>
     <h2>상품 상세 정보</h2>
     <product-read-form v-if="product" :product="product" />
-    <button>구매</button>
-    <button>삭제</button>
-    <button>수정</button>
-    <button>목록으로</button>
+    <v-btn>구매</v-btn>
+    <v-btn @click="productDelete">삭제</v-btn>
+    <v-btn @click="productModifyPage">수정</v-btn>
+    <v-btn @click="productListPage">목록으로</v-btn>
   </div>
 </template>
 <script>
 import ProductReadForm from "@/components/product/ProductReadForm.vue";
 import { mapActions, mapState } from "vuex";
+import router from "@/router";
 
 const productModule = "productModule";
 
 export default {
   name: "ProductReadPage",
+  props: {
+    id: {
+      type: String,
+      required: true,
+    },
+  },
   components: {
     ProductReadForm,
   },
@@ -24,7 +31,31 @@ export default {
     ...mapState(productModule, ["product"]),
   },
   methods: {
-    ...mapActions(productModule, ["requestProductToSpring"]),
+    ...mapActions(productModule, [
+      "requestProductToSpring",
+      "requestDeletProductToSpring",
+    ]),
+    created() {
+      this.requestProductToSpring(this.id);
+    },
+    productListPage() {
+      router.push("/productListPage");
+    },
+    productModifyPage() {
+      router.push("/productModifyPage");
+    },
+    productModifyPage() {
+      router.push("/productModifyPage");
+    },
+    async productDelete() {
+      if (!confirm("정말 삭제하시겠습니까?")) {
+        console.log("삭제하지 않습니다.");
+      } else {
+        await alert("확인(예)을 누르셨습니다.");
+        await this.requestDeletProductToSpring(this.id);
+        await this.productListPage();
+      }
+    },
   },
 };
 </script>
