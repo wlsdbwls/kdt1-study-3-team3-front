33c331c6c7fbd7ed9cb2d67affa85bff644408c4
jeongdawun <jeongdawun33@gmail.com>
2023-05-29 15:45:47 +0900

    fix: [정다운] 상세페이지 읽기 기능 오류 수정 [FPB-3]

--- src/views/product/ProductReadPage.vue
+++ src/views/product/ProductReadPage.vue
@@ -2,8 +2,9 @@
   <div>
     <h1>상품 상세페이지</h1>
     <h2>상품 상세 정보</h2>
-    <product-read-form v-if="product" :product="product" />
-    <v-btn>구매</v-btn>
+    <product-read-form v-if="product" :product="product"/>
+    <p v-else>로딩중 .......</p>
+    <v-btn @click="productOrder">구매</v-btn>
     <v-btn @click="productDelete">삭제</v-btn>
     <v-btn @click="productModifyPage">수정</v-btn>
     <v-btn @click="productListPage">목록으로</v-btn>
@@ -13,6 +14,7 @@
 import ProductReadForm from "@/components/product/ProductReadForm.vue";
 import { mapActions, mapState } from "vuex";
 import router from "@/router";
+import axiosInst from "@/utility/axiosInst";
 
 const productModule = "productModule";
 
@@ -33,13 +35,10 @@ export default {
   methods: {
     ...mapActions(productModule, [
       "requestProductToSpring",
-      "requestDeletProductToSpring",
+      "requestDeleteProductToSpring",
     ]),
     productListPage() {
       router.push("/productListPage");
-    },
-    productModifyPage() {
-      router.push("/productModifyPage");
     },
     productModifyPage() {
       router.push("/productModifyPage");
@@ -49,14 +48,20 @@ export default {
         console.log("삭제하지 않습니다.");
       } else {
         await alert("확인(예)을 누르셨습니다.");
-        await this.requestDeletProductToSpring(this.id);
+        await this.requestDeleteProductToSpring(this.id);
         await this.productListPage();
       }
     },
+    productOrder () {
+      axiosInst.post("/orders/register", {productId: this.id, userToken: localStorage.getItem("userToken")})
+      router.push("/productPurchaseCheckPage");
+    }
   },
   created() {
       this.requestProductToSpring(this.id);
   },
 };
 </script>
-<style lang=""></style>
+
+<style lang="">
+</style>
