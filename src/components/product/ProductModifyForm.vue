0d5708e7bad1df0dd81e3ed325c6c7933553c536
JiwonChoi <128335544+cjw0824@users.noreply.github.com>
2023-05-30 11:00:48 +0900

    style:[최지원] 상세페이지 css 추가

--- src/components/product/ProductModifyForm.vue
+++ src/components/product/ProductModifyForm.vue
@@ -2,34 +2,34 @@
     <div>
         <form @submit.prevent="onSubmit">
             <h1수정폼</h1>
-            <table>
+            <table class="detailbox1">
                 <tr>
-                    <td>상품 번호</td>
+                    <td class="textstyle1">상품 번호</td>
                     <td>
                         <input type="text" :value="product.id" disabled/>
                     </td>
                 </tr>
                 <tr>
-                    <td>상품명</td>
+                    <td class="textstyle1">상품명</td>
                     <td>
                         <input type="text" :v-model="product.productName"/>
                     </td>
                 </tr>
                 <tr>
-                    <td>상품 가격</td>
+                    <td class="textstyle1">상품 가격</td>
                     <td>
                         <input type="number" v-model="product.productPrice">
                     </td>
                 </tr>
                 <tr>
-                    <td>상품 정보</td>
+                    <td class="textstyle1">상품 정보</td>
                     <td>
                         <input type="text" v-model="product.productInfo">
                     </td>
                 </tr>
             </table>
             <div>
-                <button type="submit">수정</button>
+                <v-btn type="submit" class="detailbox1" color="#f18893">수정완료</v-btn>
                 <router-link :to="{
                     name: 'ProductReadPage',
                     params: { id: product.id.toString() }
@@ -69,6 +69,18 @@ export default {
     }
 }
 </script>
-<style lang="">
+<style scoped>
+    .detailbox1 {
+    margin: 70px;
+    }
+    table td {
+    padding-left: 20px;
+    padding-right: 20px;
+    padding-top: 5px;
+    }
+    .textstyle1 {
+    color: #f18893;
+    font-weight: bold;
+    }
     
 </style>
--- src/views/product/ProductReadPage.vue
+++ src/views/product/ProductReadPage.vue
@@ -1,35 +1,25 @@
 <template lang="">
   <div>
-    <h2 style="text-align: center">상품 상세 정보</h2>
-    <product-read-form v-if="product" :product="product"/>
+    <product-read-form v-if="product" :product="product" />
     <p v-else>로딩중 ...</p>
     <v-container fluid>
       <v-row justify="center">
         <v-col cols="auto">
-          <v-btn  @click="productOrder">구매</v-btn>
+          <v-btn @click="productOrder" color="#f18893">구매</v-btn>
         </v-col>
         <v-col cols="auto">
-          <v-btn @click="productDelete">삭제</v-btn>
+          <v-btn @click="productDelete" color="#f18893">삭제</v-btn>
         </v-col>
         <v-col cols="auto">
-          <v-btn @click="productModifyPage">수정</v-btn>
+          <v-btn @click="productModifyPage" color="#f18893">수정</v-btn>
         </v-col>
       </v-row>
       <v-row justify="center">
         <v-col cols="1">
-          <v-btn @click="productListPage">목록으로</v-btn>
+          <v-btn @click="productListPage" color="#f18893">목록으로</v-btn>
         </v-col>
       </v-row>
     </v-container>
-
-
-    <!-- <h2>상품 상세 정보</h2>
-    <product-read-form v-if="product" :product="product"/>
-    <p v-else>로딩중 .......</p>
-    <v-btn @click="productOrder">구매</v-btn>
-    <v-btn @click="productDelete">삭제</v-btn>
-    <v-btn @click="productModifyPage">수정</v-btn>
-    <v-btn @click="productListPage">목록으로</v-btn> -->
   </div>
 </template>
 <script>
@@ -63,7 +53,7 @@ export default {
       router.push("/productListPage");
     },
     productModifyPage() {
-      this.$router.push({name: 'ProductModifyPage', params: {id:this.id}})
+      this.$router.push({ name: "ProductModifyPage", params: { id: this.id } });
     },
     async productDelete() {
       if (!confirm("정말 삭제하시겠습니까?")) {
@@ -74,22 +64,25 @@ export default {
         await this.productListPage();
       }
     },
-    productOrder () {
-      axiosInst.post("/orders/register", {productId: this.id, userToken: localStorage.getItem("userToken")})
-      .then((res) => {
-        if(res.data === true) {
-          router.push("/productPurchaseCheckPage");
-        } else {
-          alert("사업자는 구매가 불가합니다!")
-        }
-      })
-    }
+    productOrder() {
+      axiosInst
+        .post("/orders/register", {
+          productId: this.id,
+          userToken: localStorage.getItem("userToken"),
+        })
+        .then((res) => {
+          if (res.data === true) {
+            router.push("/productPurchaseCheckPage");
+          } else {
+            alert("사업자는 구매가 불가합니다!");
+          }
+        });
+    },
   },
   created() {
-      this.requestProductToSpring(this.id);
+    this.requestProductToSpring(this.id);
   },
 };
 </script>
 
-<style lang="">
-</style>
+<style lang=""></style>
